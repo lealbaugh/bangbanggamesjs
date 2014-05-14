45e78e4515807cfb059c36be31f5e3976ade9bf3
lealbaugh <l.e.albaugh@gmail.com>
2014-05-14 13:49:25 -0400

    Support for multiple gamemodes.

--- bangbang.js
+++ bangbang.js
@@ -19,16 +19,7 @@ var io = require('socket.io').listen(server);
 io.set('log level', 1);
 
 
-//Mongo setup: if I'm running this locally, apikeys.js contains my keys; on Heroku, it's an env variable.
-var mongoUri;
-try {
-	var keys = require("./apikeys.js");
-	mongoUri = keys.mongoURL;
-}
-catch (e) {
-	mongoUri = process.env.MONGOHQ_URL;
-}
-var mongo = require('mongodb'); //https://npmjs.org/package/mongodb
+
 
 
 // Add a connect listener
@@ -44,6 +35,58 @@ io.sockets.on('connection', function(socket){
 		socket.join(roomname);
 	});
 });
+
+
+// --------- Mongo-related functions -----------
+//Setup: if I'm running this locally, apikeys.js contains my keys; on Heroku, it's an env variable.
+var mongoUri;
+try {
+	var keys = require("./apikeys.js");
+	mongoUri = keys.mongoURL;
+}
+catch (e) {
+	mongoUri = process.env.MONGOHQ_URL;
+}
+var mongo = require('mongodb'); //https://npmjs.org/package/mongodb
+
+
+function putInDB(collection, record) {
+	mongo.Db.connect(mongoUri, function (err, db) {
+	    if (err) {
+	    	console.log(err);
+	    }
+	    db.collection(collection).insert(record, function (err, docs) {
+	    	if (err) {
+	    		console.log(err);
+	    	}
+	    	else {
+	    		console.log("Inserted document into collection "+collection);
+	    	}
+	    });
+	  });
+}
+
+
+// -------- Functions for mapping activity ------------
+var geocoder = require('geocoder');
+function participantMap(content) {
+	geocoder.geocode(content, function ( err, data ) {
+		if (err) {
+			var record = {};
+		}
+		if (data) {
+			if (data.status == "OK") {
+				console.log(data.results[0].geometry)
+				var latitude = data.results[0].geometry.location.lat;
+				var longitude = data.results[0].geometry.location.lng;
+				var placename = content;
+				var record = {"placename":placename, "latitude": latitude, "longitude": longitude, "status":"OK"};
+				putInDB('geocodes', record);
+				io.sockets.in("map").emit("geocode", record);
+			}
+		}
+	});
+}
 
 // For the map, the client sends out a request for all the existing locations;
 // pull them from the "geocodes" and emit them out
@@ -65,45 +108,21 @@ function sendOutExistingLocations(socket) {
 	});
 }
 
-function putInDB(collection, record) {
-	mongo.Db.connect(mongoUri, function (err, db) {
-	    if (err) {
-	    	console.log(err);
-	    }
-	    db.collection(collection).insert(record, function (err, docs) {
-	    	if (err) {
-	    		console.log(err);
-	    	}
-	    	else {
-	    		console.log("Inserted document into collection "+collection);
-	    	}
-	    });
-	  });
-}
-
-var geocoder = require('geocoder');
-
+// -------- Handle Twilio POST ---------------------
 app.post('/twilio', function(req, res) {
 	var content = req.body.Body;
 	console.log(content);
 	if (content) {
-		geocoder.geocode(content, function ( err, data ) {
-			if (err) {
-				var record = {};
-			}
-			if (data) {
-				if (data.status == "OK") {
-					console.log(data.results[0].geometry)
-					var latitude = data.results[0].geometry.location.lat;
-					var longitude = data.results[0].geometry.location.lng;
-					var placename = content;
-					var record = {"placename":placename, "latitude": latitude, "longitude": longitude, "status":"OK"};
-					putInDB('geocodes', record);
-					io.sockets.in("map").emit("geocode", record);
+		mongo.Db.connect(mongoUri, function (err, db) {
+			db.collection("game").find({"status":"active"}).nextObject(function (err, doc) {
+				var gamemode = doc.mode;
+				console.log(doc.mode);
+				switch (gamemode) {
+					case "map":
+						participantMap(content);
 				}
-			}
-		});
-		
+			});
+		});	
 	} 
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.end();
